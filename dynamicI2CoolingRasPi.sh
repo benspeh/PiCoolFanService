431d9a09cd095f4edf6dc89699e2415ad17ab9ca
benspeh <benspeh@googlemail.com>
2024-10-27 02:23:15 +0200

    Update dynamicI2CoolingRasPi.sh

--- dynamicI2CoolingRasPi.sh
+++ dynamicI2CoolingRasPi.sh
@@ -1,11 +1,15 @@
-# Raspberry cooling i2c module including environmental temperature, cpu temperature and fan controle
+# Raspberry cooling i2c module including environmental temperature, cpu temperature and fan controle on ubuntu core 24
 
 ## activate i2c bus 
-# sudo snap install pi-config
-# sudo pi-config set pi-config.i2c 1
+# sudo snap install i2ctools --edge
+# snap connections i2ctools
+# snap interfaces|grep i2c
+# snap connect i2ctools:i2c pi:i2c-1
 # sudo reboot
+# sudo i2ctools.i2cdetect -y 1
 
-# sudo i2cdetect -y 1
+# sudo i2ctools.i2cdetect -y 1
+#https://www.pimodules.com/_pdf/PCFM_V1.05.pdf
 
 #!/bin/bash
 #dynamische temp steuerung + Log
@@ -14,14 +18,14 @@ function fan_on () {
 #cpu temperature and dynamic
    local t1=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
 
-   if [ $t1 -ge 45 -a $t1 -le 50 ]; then
-       sudo i2cset -y 1 0x6C 1 2
+   if [ $t1 -ge 46 -a $t1 -le 50 ]; then
+       sudo i2ctools.i2cset -y 1 0x6C 1 2
    elif [ $t1 -ge 51 -a $t1 -le 55 ]; then
-       sudo i2cset -y 1 0x6C 1 3
+       sudo i2ctools.i2cset -y 1 0x6C 1 3
    elif [ $t1 -ge 56 -a $t1 -le 60 ]; then
-       sudo i2cset -y 1 0x6C 1 4
+       sudo i2ctools.i2cset -y 1 0x6C 1 4
    elif [ $t1 -gt 60 ]; then
-       sudo i2cset -y 1 0x6C 1 1
+       sudo i2ctools.i2cset -y 1 0x6C 1 1
    fi
 }
 
@@ -29,20 +33,20 @@ function fan_off () {
    local t1=$(($(cat /sys/class/thermal/thermal_zone0/temp)/1000))
 
    if [ $t1 -le 40 ]; then
-       sudo i2cset -y 1 0x6C 1 0
+       sudo i2ctools.i2cset -y 1 0x6C 1 0
    fi
 }
 
 function log () {
 
    local ts2=`date +%F_%H-%M-%S`
-   local f1=$`sudo i2cget -y 1 0x6C 1 b`
+   local f1=$`sudo i2ctools.i2cget -y 1 0x6C 1 c`
    local f1=${f1:4:1}
    local t1=`cat /sys/class/thermal/thermal_zone0/temp`
    local t1=`echo "scale=2; "\`echo ${t1##*=}\`" / 1000" | bc`
    local t2=`vcgencmd measure_temp`
    local t2=${t2:5:4}
-   local t3=$`sudo i2cget -y 1 0x6C 2 b`
+   local t3=$`sudo i2ctools.i2cget -y 1 0x6C 2 c`
    local t3=${t3:3:2}
 
 
@@ -81,8 +85,8 @@ function log () {
 }
 
 # set unconditional FAN ON
-sudo i2cset -y 1 0x6C 0 1;
-
+sudo i2ctools.i2cset -y 1 0x6C 0 1;
+sudo i2ctools.i2cset -y 1 0x6C 4 46
 while true
 do
 
