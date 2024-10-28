1277065770a36551daffc32111a950a8fb05fd4b
benspeh <benspeh@googlemail.com>
2024-10-28 13:22:34 +0100

    Update Update_dynamicI2CoolingService.sh

--- Update_dynamicI2CoolingService.sh
+++ Update_dynamicI2CoolingService.sh
@@ -2,8 +2,11 @@ cd /home/my-git-repositories/RaspIrrigation
 
 sudo docker run --rm -v "$PWD":/repo -w /repo alpine/git pull origin main
 
+sudo systemctl stop dynamicI2Cooling.service
+
 sudo cp dynamicI2Cooling.service /etc/systemd/system/dynamicI2Cooling.service
 sudo chmod +x /home/my-git-repositories/RaspIrrigation/dynamicI2CoolingRasPi.sh
+
 sudo systemctl daemon-reload
 sudo systemctl restart dynamicI2Cooling.service
 sudo systemctl status dynamicI2Cooling.service
