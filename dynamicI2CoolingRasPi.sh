cfdb13bd658729cc34af34c6ec69cfb9472fb5f1
benspeh <benspeh@googlemail.com>
2024-10-28 14:29:53 +0100

    Update dynamicI2CoolingRasPi.sh

--- dynamicI2CoolingRasPi.sh
+++ dynamicI2CoolingRasPi.sh
@@ -45,9 +45,9 @@ function log () {
    local ts2=`date +%F_%H-%M-%S`
    local f1=$`sudo i2ctools.i2cget -y 1 0x6C 1 c`
    local f1=${f1:4:1}
-   local t1=`cat "$temperature_cpu"`
-   local t1=`echo "scale=2; "\`echo ${t1##*=}\`" / 1000" | bc`
-
+   local t1_get=`cat "$temperature_cpu"`
+   t1_value=${t1_get##*=}
+   t1=$(echo "$t1_value / 1000" | awk '{ printf "%.2f", $1 }')
    local t2=$`sudo i2ctools.i2cget -y 1 0x6C 2 c`
    local t2=${t2:3:2}
 
