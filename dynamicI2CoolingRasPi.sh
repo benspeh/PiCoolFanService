4a63c30e2cd6ff003f74d9b8ec09f178b0cd25ac
benspeh <benspeh@googlemail.com>
2024-11-16 18:45:37 +0100

    Update dynamicI2CoolingRasPi.sh

--- dynamicI2CoolingRasPi.sh
+++ dynamicI2CoolingRasPi.sh
@@ -42,13 +42,13 @@ function fan_off () {
 function log () {
 
    local ts2=`date +%F_%H-%M-%S`
-   local f1=$`sudo i2ctools.i2cget -y 1 0x6C 1 c`
+   local f1=$`i2ctools.i2cget -y 1 0x6C 1 c`
    local f1=${f1:4:1}
    
    local t1_get=`cat "$temperature_cpu"`
    local t1=$(awk "BEGIN { printf \"%.2f\", $t1_get / 1000 }")
    
-   local t2=$`sudo i2ctools.i2cget -y 1 0x6C 2 c`
+   local t2=$`i2ctools.i2cget -y 1 0x6C 2 c`
    local t2=${t2:3:2}
 
 
