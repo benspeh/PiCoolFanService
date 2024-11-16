42ea499f7a78f4c5aa90b910e1cb91425d113c2c
benspeh <benspeh@googlemail.com>
2024-11-16 23:26:32 +0100

    Update dynamicI2CoolingRasPi.sh

--- dynamicI2CoolingRasPi.sh
+++ dynamicI2CoolingRasPi.sh
@@ -25,10 +25,10 @@ function read_values() {
     local t1=$(awk "BEGIN { printf \"%.2f\", $t1_get / 1000 }")  # CPU temperature in °C
     
     local t2=$(i2ctools.i2cget -y 1 0x6C 2 c)
-    local t2=${t2:2}  
+    local t2=${t2:1}  
 
     local f1=$(i2ctools.i2cget -y 1 0x6C 1 c)
-    local f1=${f1:4:1}  
+    local f1=${f1:3:1}  
 
 
     # Return the values
